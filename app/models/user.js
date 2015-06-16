7851667e24ae6fcfaae988978d0c846beaafabb4
Diego García <diego@lateralview.net>
2015-06-16 13:09:36 -0300

    Add shortid

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose"),
   Schema = mongoose.Schema,
   bcrypt = require("bcrypt-nodejs"),
-  crypto = require('crypto');
+  shortid = require('shortid');
 
 var validateEmail = function(email) {
   var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -12,7 +12,7 @@ var validateEmail = function(email) {
 var UserSchema = new Schema({
   email: { type: String, trim: true, required: "Email is required.", index: { unique: true }, validate: [validateEmail, "Please fill a valid email address."]},
   password: { type: String, required: "Password is required.", select: false, minlength: [8, "Password is too short." ] },
-  activation_token: { type: String, select: false, index: { unique: true } },
+  activation_token: { type: String, select: false, unique: true, 'default': shortid.generate },
   active: { type: Boolean, default: true, select: false }, // TODO: activate account by email
   created_at: { type: Date, default: Date.now }
 });
@@ -27,21 +27,6 @@ UserSchema.pre("save", function(next) {
     user.password = hash;
     next();
   });
-});
-
-// generate activation token before user is created
-UserSchema.pre("save", function(next) {
-  var user = this;
-
-  if (user.isNew) {
-    crypto.randomBytes(48, function(ex, buf) {
-      user.activation_token = buf.toString('hex');
-      next();
-    });
-  }
-  else {
-    next();
-  }
 });
 
 // method to compare a given password with the database hash
