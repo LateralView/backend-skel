81f75caf51ceddb6b34787a1f48c8debadeb40cf
Diego García <diego@lateralview.net>
2015-11-27 16:00:31 -0300

    Clean test database with async module

--- test/utils.js
+++ test/utils.js
@@ -2,26 +2,32 @@ process.env.NODE_ENV = 'test';
 
 var mongoose = require('mongoose');
 var config = require('../config').config();
+var async = require("async");
 
 before(function (done) {
 
- function clearDB() {
-   for (var i in mongoose.connection.collections) {
-     mongoose.connection.collections[i].remove(function() {});
-   }
-   return done();
- }
+  function clearDB() {
+    async.each(mongoose.connection.collections, function(collection, callback) {
+      collection.remove(function() {
+        callback();
+      });
+    }, function(err){
+      if(err)
+        throw err;
+      else
+        return done();
+    });
+  }
 
- if (mongoose.connection.readyState === 0) {
-   mongoose.connect(config.database, function (err) {
-     if (err) {
-       throw err;
-     }
-     return clearDB();
-   });
- } else {
-   return clearDB();
- }
+  if (mongoose.connection.readyState === 0) {
+    mongoose.connect(config.database, function (err) {
+      if (err)
+        throw err;
+      return clearDB();
+    });
+  } else
+      return clearDB();
+
 });
 
 after(function (done) {
