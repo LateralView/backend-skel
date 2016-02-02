453997eee9ab4e96545e6dfc2d0812bebb5e8cdd
Diego García <diego@lateralview.net>
2016-02-02 16:48:17 -0300

    Improve s3Manager tests

--- test/helpers/s3Manager.js
+++ test/helpers/s3Manager.js
@@ -20,9 +20,10 @@ describe('s3Manager Helper', function () {
 
     });
 
-	it('uploads file and returns file path', function (done) {
-		// Mock s3 response
-		nock('https://mean-skel.s3.amazonaws.com:443')
+    describe("upload file", function(){
+    	it('returns file path if upload success', function (done) {
+			// Mock s3 response
+			nock('https://mean-skel.s3.amazonaws.com:443')
 				.put(/.*picture*./)
 				.reply(200, "", { 'x-amz-id-2': '6pv/eHWz7VrUPAJNr15F3OzFiXIFi/QJU0UArw3pG7/xYSh5LaX+8RQDelmFp61bYuHvWXTJaWs=',
 					'x-amz-request-id': '3F74105A9E031597',
@@ -32,20 +33,114 @@ describe('s3Manager Helper', function () {
 					server: 'AmazonS3',
 					connection: 'close' });
 
-		var file = {
-			name: "avatar.png",
-			path: "./test/fixtures/avatar.png",
-			mimetype: "image/png"
-		};
+			var file = {
+				name: "avatar.png",
+				path: "./test/fixtures/avatar.png",
+				mimetype: "image/png"
+			};
 
-		s3Manager.uploadFile(file, "picture/" + validUser._id, function(err, path) {
-			nock.cleanAll();
-			expect(err).to.not.exist;
-			expect(path).to.eq("picture/" + validUser._id + "/avatar.png");
-		    done();
-		});
+			s3Manager.uploadFile(file, "picture/" + validUser._id, function(err, path) {
+				nock.cleanAll();
+				expect(err).to.not.exist;
+				expect(path).to.eq("picture/" + validUser._id + "/avatar.png");
+			    done();
+			});
+	    });
 
+	    it('returns error if response is an error', function (done) {
+			nock('https://mean-skel.s3.amazonaws.com:443')
+			  .put(/.*picture*./)
+			  .reply(403, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Error><Code>InvalidAccessKeyId</Code><Message>The AWS Access Key Id you provided does not exist in our records.</Message><AWSAccessKeyId>TEST_KEY</AWSAccessKeyId><RequestId>19F25D5603FE3C5D</RequestId><HostId>SFpA1tktdCW8iEhMsEgeTLaYf8ZIAVq6EtYWU44ZhM/3qsy9bTP8gswERuVJjX48aHq5vU+jQGQ=</HostId></Error>", { 'x-amz-request-id': '19F25D5603FE3C5D',
+			  'x-amz-id-2': 'SFpA1tktdCW8iEhMsEgeTLaYf8ZIAVq6EtYWU44ZhM/3qsy9bTP8gswERuVJjX48aHq5vU+jQGQ=',
+			  'content-type': 'application/xml',
+			  'transfer-encoding': 'chunked',
+			  date: 'Tue, 02 Feb 2016 19:36:15 GMT',
+			  server: 'AmazonS3',
+			  connection: 'close' });
+
+			var file = {
+				name: "avatar.png",
+				path: "./test/fixtures/avatar.png",
+				mimetype: "image/png"
+			};
+
+			s3Manager.uploadFile(file, "picture/" + validUser._id, function(err, path) {
+				nock.cleanAll();
+				expect(err).to.exist;
+				expect(err.message).to.equal('The AWS Access Key Id you provided does not exist in our records.');
+				done();
+			});
+	    });
+
+    	it('returns error if the request fails', function (done) {
+			nock('https://mean-skel.s3.amazonaws.com:443')
+			  .put(/.*picture*./)
+			  .replyWithError('Some error');
+
+			var file = {
+				name: "avatar.png",
+				path: "./test/fixtures/avatar.png",
+				mimetype: "image/png"
+			};
+
+			s3Manager.uploadFile(file, "picture/" + validUser._id, function(err, path) {
+				nock.cleanAll();
+				expect(err).to.exist;
+				done();
+			});
+	    });
 
     });
+
+    describe("delete file", function(){
+    	it('deletes file successfully', function (done) {
+			nock('https://mean-skel.s3.amazonaws.com:443')
+			  .delete(/.*picture*./)
+			  .reply(204, "", { 'x-amz-id-2': 'zfAvIl4BX0ILfVzehFzEj8hzxhJKQBhWISbU0QoWTuZHUu7R5MtmH+SHK65rgxRiEXfMoRRIsXQ=',
+				  'x-amz-request-id': '4681858C4C9D2F51',
+				  date: 'Tue, 02 Feb 2016 18:15:39 GMT',
+				  server: 'AmazonS3',
+				  connection: 'close' });
+
+			s3Manager.deleteFile("picture/56b0f1d8a60d504834ffe605/avatar.png", function(err) {
+				nock.cleanAll();
+				expect(err).to.not.exist;
+				done();
+			});
+	    });
+
+	    it('returns error if response is an error', function (done) {
+			nock('https://mean-skel.s3.amazonaws.com:443')
+			  .delete(/.*picture*./)
+			  .reply(403, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Error><Code>InvalidAccessKeyId</Code><Message>The AWS Access Key Id you provided does not exist in our records.</Message><AWSAccessKeyId>TEST_KEY</AWSAccessKeyId><RequestId>19F25D5603FE3C5D</RequestId><HostId>SFpA1tktdCW8iEhMsEgeTLaYf8ZIAVq6EtYWU44ZhM/3qsy9bTP8gswERuVJjX48aHq5vU+jQGQ=</HostId></Error>", { 'x-amz-request-id': '19F25D5603FE3C5D',
+			  'x-amz-id-2': 'SFpA1tktdCW8iEhMsEgeTLaYf8ZIAVq6EtYWU44ZhM/3qsy9bTP8gswERuVJjX48aHq5vU+jQGQ=',
+			  'content-type': 'application/xml',
+			  'transfer-encoding': 'chunked',
+			  date: 'Tue, 02 Feb 2016 19:36:15 GMT',
+			  server: 'AmazonS3',
+			  connection: 'close' });
+
+			s3Manager.deleteFile("picture/56b0f1d8a60d504834ffe605/avatar.png", function(err) {
+				nock.cleanAll();
+				expect(err).to.exist;
+				expect(err.message).to.equal('The AWS Access Key Id you provided does not exist in our records.');
+				done();
+			});
+	    });
+
+	    it('returns error if the request fails', function (done) {
+			nock('https://mean-skel.s3.amazonaws.com:443')
+			  .delete(/.*picture*./)
+			  .replyWithError('Some error');
+
+			s3Manager.deleteFile("picture/56b0f1d8a60d504834ffe605/avatar.png", function(err) {
+				nock.cleanAll();
+				expect(err).to.exist;
+				done();
+			});
+	    });
+
+    });
+
 
 });
