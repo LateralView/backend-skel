872d98d480e2ccaa6c04bd82d513a50c0ff1e75b
Gabriel Casullo <info@casu-net.com.ar>
2017-03-17 11:15:13 -0300

    migrate models to ES6

--- app/models/plugins/foregroundIndexesPlugin.js
+++ app/models/plugins/foregroundIndexesPlugin.js
@@ -1,4 +1,4 @@
-module.exports = function foregroundIndexesPlugin (schema) {
+module.exports = (schema) => {
 	// Build indexes in foreground for test environment
 	if (process.env.NODE_ENV === 'test'){
 		schema.indexes().forEach(function(index){
