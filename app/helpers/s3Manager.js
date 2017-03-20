228786f8c05824a5ec64eba28b3680cea9efb326
Diego García <diego@lateralview.net>
2017-03-20 14:31:11 -0300

    Fix s3Manager file upload

--- app/helpers/s3Manager.js
+++ app/helpers/s3Manager.js
@@ -10,24 +10,28 @@ aws.config.update({
 aws.config.region = config.aws.region;
 
 function uploadFile(file, path, next) {
-	try {
-		var filePath = path + "/" + file.name;
-		var body = fs.createReadStream(file.path);
+	var filePath = path + "/" + file.name;
+	var body = fs.createReadStream(file.path);
 
-		// Handle read stream error
-		body.on('error', function(error){ next(error); });
+	// Handle read stream error
+	body.on('error', function(error){
+		return next(error);
+	});
 
-		var s3obj = new aws.S3({params: {Bucket: config.aws.S3_BUCKET_NAME, Key: filePath}});
-		s3obj.upload({Body: body, ContentType: file.mimetype}).
-		  send(function(err, data) {
-		  	if (err)
-				next(err);
-			else
-				next(null, filePath);
-		  });
-	} catch (err) {
-		next(err);
-	}
+	body.on('open', function (){
+		try {
+			var s3obj = new aws.S3({params: {Bucket: config.aws.S3_BUCKET_NAME, Key: filePath}});
+			s3obj.upload({Body: body, ContentType: file.mimetype}).
+			  send(function(err, data) {
+			  	if (err)
+					next(err);
+				else
+					next(null, filePath);
+			  });
+		} catch (err) {
+			next(err);
+		}
+	});
 }
 
 function deleteFile(key, next) {
--- test/helpers/s3Manager.js
+++ test/helpers/s3Manager.js
@@ -50,17 +50,6 @@ describe('s3Manager Helper', function () {
 	    });
 
 	    it('returns error if file does not exist', function (done) {
-			// Mock s3 response
-			nock('https://mean-skel.s3.amazonaws.com:443')
-				.put(/.*picture*./)
-				.reply(200, "", { 'x-amz-id-2': '6pv/eHWz7VrUPAJNr15F3OzFiXIFi/QJU0UArw3pG7/xYSh5LaX+8RQDelmFp61bYuHvWXTJaWs=',
-					'x-amz-request-id': '3F74105A9E031597',
-					date: 'Tue, 02 Feb 2016 14:14:33 GMT',
-					etag: '"21a280f3002ffdf828edd9b56eef380f"',
-					'content-length': '0',
-					server: 'AmazonS3',
-					connection: 'close' });
-
 			var file = {
 				name: "avatar.png",
 				path: "./invalid/path/image.png",
@@ -68,7 +57,6 @@ describe('s3Manager Helper', function () {
 			};
 
 			s3Manager.uploadFile(file, "picture/" + validUser._id, function(err, path) {
-				nock.cleanAll();
 				expect(err).to.exist;
 				expect(err.code).to.equal("ENOENT");
 			    done();
@@ -117,18 +105,18 @@ describe('s3Manager Helper', function () {
 				done();
 			});
 	    });
-    
+
         it('returns error if aws sdk throw error', function (done) {
             nock('https://mean-skel.s3.amazonaws.com:443')
                 .put(/.*picture*./)
                 .replyWithError('Some error');
-        
+
             var file = {
                 name: "avatar.png",
                 path: "./test/fixtures/avatar.png",
                 mimetype: "image/png"
             };
-            
+
             var stub = sinon.stub(aws, 'S3', function () {
 				return {
 					upload: function () {
@@ -136,7 +124,7 @@ describe('s3Manager Helper', function () {
                     }
 				}
             });
-        
+
             s3Manager.uploadFile(file, "picture/" + validUser._id, function(err, path) {
                 nock.cleanAll();
                 stub.restore();
@@ -193,18 +181,18 @@ describe('s3Manager Helper', function () {
 				done();
 			});
 	    });
-    
+
         it('returns error if aws sdk throw error', function (done) {
             nock('https://mean-skel.s3.amazonaws.com:443')
                 .put(/.*picture*./)
                 .replyWithError('Some error');
-        
+
             var file = {
                 name: "avatar.png",
                 path: "./test/fixtures/avatar.png",
                 mimetype: "image/png"
             };
-        
+
             var stub = sinon.stub(aws, 'S3', function () {
                 return {
                     deleteObject: function () {
@@ -212,7 +200,7 @@ describe('s3Manager Helper', function () {
                     }
                 }
             });
-        
+
             s3Manager.deleteFile("picture/56b0f1d8a60d504834ffe605/avatar.png", function(err, path) {
                 nock.cleanAll();
                 stub.restore();
